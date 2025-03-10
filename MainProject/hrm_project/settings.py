cef9c1e1d2c041d3b9b5b2ec20130b2927a20ec2
Pranavprj <jadhavpranav64@gmail.com>
2025-03-10 20:25:19 +0530

    user app and login pages and register pages

--- MainProject/hrm_project/settings.py
+++ MainProject/hrm_project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'department',
     'role',
     'dashboard',
+    'user',
 ]
 
 MIDDLEWARE = [
@@ -71,7 +72,7 @@ TEMPLATES = [
         },
     },
 ]
-
+AUTH_USER_MODEL = 'user.User'
 WSGI_APPLICATION = 'hrm_project.wsgi.application'
 
 
@@ -120,8 +121,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+import os
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 
 MEDIA_URL = 'media/'        
 MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
--- MainProject/user/models.py
+++ MainProject/user/models.py
@@ -1,10 +1,9 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
-# Create your models here.
 class User(AbstractUser):
-    ROLE_CHOICES = (
-        ('admin', 'Admin'),
-        ('employee', 'Employee'),
-    )
-    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+    # Add any custom fields here if needed
+    phone_number = models.CharField(max_length=15, unique=True, null=True, blank=True)
+
+    def __str__(self):
+        return self.username

01977c6681489b7938a0a507ba1a5117fb4f06a1
Pranavprj <jadhavpranav64@gmail.com>
2025-03-21 09:21:58 +0530

    Dashboard and Login and Register Page using React

---
+++ MainProjectFrontEnd/vibgyor/src/Components/Register.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './../css/loginregister.css'
+
+export default function Register() {
+  return (
+    <div><div className='login-register'>
+        Register
+        </div>
+      
+    </div>
+  )
+}
--- MainProjectFrontEnd/vibgyor/src/Components/Route.jsx
+++ MainProjectFrontEnd/vibgyor/src/Components/Route.jsx
@@ -1,15 +1,50 @@
 import React from 'react'
-import { createBrowserRouter } from 'react-router-dom'
-import Navbar from './Navbar'
+import { createBrowserRouter } from "react-router-dom";
+import Navbar from './Navbar';
+import Dashboard from './Dashboard';
+import Home from './Home';
+import Login from './Login';
+import Register from './Register';
 export default function Route() {
-  return createBrowserRouter ([
-    {
-        path:"/",
-        element: (
-            <>
-            <Navbar/>
-            </>
-        ),
+ return createBrowserRouter([
+   {
+     path: "/",
+     element: (
+       <>
+         <Navbar />
+         <Home/>
+       </>
+     ),
+     children:[
+      {
+      path:"/login",
+      element:(
+        <>
+        <Login/>
+        </>
+      
+      )
     },
-  ]);
+    {
+      path:"/register",
+      element:(
+        <>
+        <Register/>
+        </>
+      
+      )
+    }
+     ]
+   },
+   {
+    path: "/dashboard",
+    element: (
+      <>
+        <Navbar />
+        <Dashboard/>
+      </>
+    ),
+  },
+   
+ ]);
 }
